01e34d66fd095340f6e52b7232de63815b9ec156
Mahmoud-Da <mdabbagh6@gmail.com>
2026-01-27 12:49:07 +0900

    4- Image - #3

--- The-Ultimate-React-Native-Series-Fundamentals/2-Fundamental-Concepts/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/2-Fundamental-Concepts/done-with-it/App.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   const handlePress = () => {
     console.log('Text pressed');
   };
 
+  console.log(require('./assets/icon.png'));
+
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>
         Click me
       </Text>
+      {/* <Image source={require('./assets/icon.png')} /> */}
+
+      <Image
+        blurRadius={10}
+        source={{
+          uri: 'https://picsum.photos/200/300',
+          width: 200,
+          height: 300,
+        }}
+        loadingIndicatorSource={require('./assets/icon.png')}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -19,7 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
