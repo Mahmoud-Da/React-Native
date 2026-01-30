6b1ce763d532cb125c94bcc2ab046249decb11f6
Mahmoud-Da <mdabbagh6@gmail.com>
2026-01-30 15:56:26 +0900

    3- Detecting Orientation Changes - #4

--- The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
@@ -1,36 +1,16 @@
-import {
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-  Dimensions
-} from 'react-native';
+import React from "react";
+import { SafeAreaView, View } from "react-native";
 
 export default function App() {
-  const screen = Dimensions.get("screen");
-
-  console.log(screen);
-
   return (
     <SafeAreaView>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          // width: 150,
-          width: "50%",
+          width: 150,
           height: 70,
         }}
       />
     </SafeAreaView>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-  },
-});
