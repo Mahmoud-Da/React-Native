0675090f745c3f12220b68532edff707900d0034
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-02 12:43:39 +0900

    4- Flexbox - #4

--- The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 150,
-          height: 70,
-        }}
-      />
-    </SafeAreaView>
+    <View style={{ backgroundColor: 'white', flex: 1 }}>
+      <View style={{ backgroundColor: 'dodgerblue', flex: 1 }} />
+      <View style={{ backgroundColor: 'gold', flex: 1 }} />
+      <View style={{ backgroundColor: 'tomato', flex: 1 }} />
+    </View>
   );
 }
