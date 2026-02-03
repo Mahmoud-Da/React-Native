e3d7dc402f79cac88d4209d10059d4d1f22b2dc5
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-03 14:45:14 +0900

    11- Building the Welcome Screen - #4

--- The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/3-Layout/done-with-it/App.js
@@ -1,26 +1,7 @@
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: 'white',
-      flexDirection: 'row',
-      justifyContent: 'center', // primary Axis react-native main=column, CSS main=row
-      alignItems: 'center', // secondary Axis react-native main=row, CSS main=column
-    }}>
-      <View style={{ backgroundColor: 'dodgerblue', width: 100, height: 100 }} />
-      <View
-        style={{
-          position: 'absolute',
-          backgroundColor: 'gold',
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-        }}
-      />
-      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }} />
-    </View>
+    <WelcomeScreen />
   );
 }
