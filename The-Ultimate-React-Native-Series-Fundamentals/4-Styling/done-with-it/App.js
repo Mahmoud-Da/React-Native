74a14591c11654a70610c965a2de622da5fe9e67
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-04 16:53:48 +0900

    4- Paddings and Margins - #5

--- The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
@@ -14,21 +14,26 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50,
+          padding: 20,
+          paddingHorizontal: 10,
+          paddingLeft: 30,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+        />
+      </View>
 
-          // iOS Shadow
-          shadowColor: "gray",
-          shadowOffset: {
-            width: 0,
-            height: 10,
-          },
-          shadowOpacity: 0.5,
-          shadowRadius: 10,
-
-          // Android Shadow
-          elevation: 20,
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+          margin: 20,
         }}
       />
     </View>
