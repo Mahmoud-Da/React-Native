f5d81efced97820cd1bd42825c48ba18aaf2c879
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-05 13:24:29 +0900

    7- Icons - #5

--- The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
@@ -1,20 +1,23 @@
-import { View } from "react-native";
-import AppText from "./app/components/AppText";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20,
-      }}
-    >
-      <AppText>
-        Hello from AppText
-      </AppText>
+    <View style={styles.container}>
+      <MaterialCommunityIcons
+        name="email"
+        size={60}
+        color="dodgerblue"
+      />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
