a91a0673cbae9dc1c1f01217160c45a4c6bfb6b4
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-05 13:32:42 +0900

    8- Platform-specific Code - #5

--- The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/4-Styling/done-with-it/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MaterialCommunityIcons
-        name="email"
-        size={60}
-        color="dodgerblue"
-      />
+      <AppText>I love React Native</AppText>
     </View>
   );
 }
