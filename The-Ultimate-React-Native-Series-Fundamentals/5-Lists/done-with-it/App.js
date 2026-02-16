41096fda25280d3e359b8e3fc861db0845a6910c
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-16 12:23:12 +0900

    12- Extending the ListItem Component - #6

--- The-Ultimate-React-Native-Series-Fundamentals/5-Lists/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/5-Lists/done-with-it/App.js
@@ -1,8 +1,15 @@
-import MessagesScreen from "./app/screens/MessagesScreen";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import ListItem from "./app/components/ListItem";
+import Screen from "./app/screens/Screen.js";
+import Icon from "./app/components/Icon.js";
 
 export default function App() {
-  return (<GestureHandlerRootView style={{ flex: 1 }}> <MessagesScreen /></GestureHandlerRootView>)
-
+  return (<GestureHandlerRootView style={{ flex: 1 }}>
+    <Screen>
+      <ListItem
+        title="My title"
+        subtitle="My subtitle"
+        ImageComponent={<Icon name="email" />} />
+    </Screen>
+  </GestureHandlerRootView>)
 }
