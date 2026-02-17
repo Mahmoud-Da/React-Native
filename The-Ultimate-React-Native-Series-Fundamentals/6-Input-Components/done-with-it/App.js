38f040fb9f9e4bab4536c0ac1e9939cba8395034
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-17 16:26:32 +0900

    2- TextInput - #2

--- The-Ultimate-React-Native-Series-Fundamentals/6-Input-Components/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Fundamentals/6-Input-Components/done-with-it/App.js
@@ -1,8 +1,31 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ListingsScreen from "./app/screens/ListingsScreen.js";
+import React, { useState } from "react";
+import { TextInput, Text, StyleSheet } from "react-native";
+import Screen from "./app/components/Screen";
 
-export default function App() {
-  return (<GestureHandlerRootView style={{ flex: 1 }}>
-    <ListingsScreen />
-  </GestureHandlerRootView>)
+function App() {
+  const [firstName, setFirstName] = useState("");
+
+  return (
+    <Screen>
+      <TextInput
+        placeholder="First Name"
+        style={styles.input}
+        onChangeText={(text) => setFirstName(text)}
+        maxLength={20}
+        keyboardType="default"
+        clearButtonMode="always"
+      />
+      <Text>{firstName}</Text>
+    </Screen>
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+});
+
+export default App;
