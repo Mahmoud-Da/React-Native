5933e12d60023ab2335df1d7c155d91e5c3f5c67
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-17 16:15:44 +0900

    1- Introduction - #7

---
+++ The-Ultimate-React-Native-Series-Fundamentals/6-Input-Components/done-with-it/app/components/Icon.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function Icon({
+  name,
+  size = 40,
+  backgroundColor = "black",
+  iconColor = "white",
+}) {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          width: size,
+          height: size,
+          borderRadius: size / 2,
+          backgroundColor,
+        },
+      ]}
+    >
+      <MaterialCommunityIcons
+        name={name}
+        size={size * 0.5}
+        color={iconColor}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Icon;
