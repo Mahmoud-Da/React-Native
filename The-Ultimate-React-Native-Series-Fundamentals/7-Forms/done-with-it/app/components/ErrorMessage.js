02aa060c66c79fe5dbf2503a62ae91554ad88c97
Mahmoud-Da <mdabbagh6@gmail.com>
2026-02-23 13:37:29 +0900

    6- The Touched State - #7

--- The-Ultimate-React-Native-Series-Fundamentals/7-Forms/done-with-it/app/components/ErrorMessage.js
+++ The-Ultimate-React-Native-Series-Fundamentals/7-Forms/done-with-it/app/components/ErrorMessage.js
@@ -1,8 +1,9 @@
 import { StyleSheet } from "react-native";
 import AppText from "./AppText";
 
-function ErrorMessage({ error }) {
-  if (!error)
+function ErrorMessage({ error, visible }) {
+
+  if (!visible || !error)
     return null;
 
   return (
