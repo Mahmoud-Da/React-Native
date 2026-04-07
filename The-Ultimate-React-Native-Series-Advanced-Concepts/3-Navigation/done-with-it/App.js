d83db55f4917fca05fef89f088bca90f53d23294
Mahmoud-Da <mdabbagh6@gmail.com>
2026-04-07 13:45:09 +0900

    3- Stack Navigator - #49

--- The-Ultimate-React-Native-Series-Advanced-Concepts/3-Navigation/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Advanced-Concepts/3-Navigation/done-with-it/App.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
 
-export default function App() {
-  return <ListingEditScreen />;
-}
+const Stack = createStackNavigator();
+
+const Tweets = () => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+    </Screen>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+};
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
