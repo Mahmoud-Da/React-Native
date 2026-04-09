f8e59be46061b91c4956784874b7e75d3e86d5f9
Mahmoud-Da <mdabbagh6@gmail.com>
2026-04-09 11:54:55 +0900

    9- Customizing Tabs - #49

--- The-Ultimate-React-Native-Series-Advanced-Concepts/3-Navigation/done-with-it/App.js
+++ The-Ultimate-React-Native-Series-Advanced-Concepts/3-Navigation/done-with-it/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
 
@@ -20,9 +21,40 @@ const Account = () => (
 );
 
 const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Feed" component={Tweets} />
-    <Tab.Screen name="Account" component={Account} />
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "lightgray",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={Tweets}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            name="home"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Account"
+      component={Account}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons
+            name="account"
+            size={size}
+            color={color}
+          />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
