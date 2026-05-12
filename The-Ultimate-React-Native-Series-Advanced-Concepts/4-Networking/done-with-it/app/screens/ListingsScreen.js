f7df9a61ca0ae3e47bc27806b53fa4f868cb7fb9
Mahmoud-Da <mdabbagh6@gmail.com>
2026-05-12 11:40:04 +0900

    5- Fetching Data - #50

--- The-Ultimate-React-Native-Series-Advanced-Concepts/4-Networking/done-with-it/app/screens/ListingsScreen.js
+++ The-Ultimate-React-Native-Series-Advanced-Concepts/4-Networking/done-with-it/app/screens/ListingsScreen.js
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
+import listingsApi from "../api/listings";
 
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+const ListingsScreen = ({ navigation }) => {
+  const [listings, setListings] = useState([]);
 
-function ListingsScreen({ navigation }) {
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+
+    setListings(response.data);
+  };
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -31,14 +29,16 @@ function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            imageUrl={item.images[0].url}
+            onPress={() =>
+              navigation.navigate(routes.LISTING_DETAILS, item)
+            }
           />
         )}
       />
     </Screen>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
