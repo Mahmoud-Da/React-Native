3e066c8d8402451e53d2590140a180b17e7b1948
Mahmoud-Da <mdabbagh6@gmail.com>
2026-05-12 11:49:59 +0900

    7- Handling Errors - #50

--- The-Ultimate-React-Native-Series-Advanced-Concepts/4-Networking/done-with-it/app/screens/ListingsScreen.js
+++ The-Ultimate-React-Native-Series-Advanced-Concepts/4-Networking/done-with-it/app/screens/ListingsScreen.js
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
+import Screen from "../components/Screen";
+import Text from "../components/Text";
+import Button from "../components/Button";
+
 import colors from "../config/colors";
 import routes from "../navigation/routes";
-import Screen from "../components/Screen";
 import listingsApi from "../api/listings";
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
 
+    if (!response.ok) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
     setListings(response.data);
   };
 
@@ -22,6 +32,13 @@ const ListingsScreen = ({ navigation }) => {
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <Text>Couldn't retrieve the listings.</Text>
+          <Button title="Retry" onPress={loadListings} />
+        </>
+      )}
+
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
