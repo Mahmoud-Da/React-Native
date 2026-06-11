e0f9e3409bace3a03e7114d5fb248325c0be89bb
Mahmoud-Da <mdabbagh6@gmail.com>
2026-06-11 13:44:50 +0900

    5- AsyncStorage - #51

---
+++ The-Ultimate-React-Native-Series-Advanced-Concepts/5-Offline-Support/done-with-it/demo/5-AsyncStorage-demo.js
@@ -0,0 +1,22 @@
+import { AsyncStorage } from "react-native";
+
+const demo = async () => {
+  try {
+    await AsyncStorage.setItem(
+      "person",
+      JSON.stringify({
+        id: 1,
+      })
+    );
+
+    const value = await AsyncStorage.getItem("person");
+
+    const person = JSON.parse(value);
+
+    console.log(person);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+demo();
